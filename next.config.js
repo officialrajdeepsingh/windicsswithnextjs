fecb9a3db6b4559f9200901fc262c4a4ea13bdf8
officialrajdeepsingh <radewala.2@gmail.com>
2022-11-17 18:06:57 +0530

    update the next.config.j

--- next.config.js
+++ next.config.js
@@ -1,26 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
 
 
 module.exports = {
+
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
     return config
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  assetPrefix: "/windicsswithnextjs",
+  basePath: "/windicsswithnextjs",
   images: {
     loader: 'imgix',
     path: 'the "domain" of your Imigix source',
