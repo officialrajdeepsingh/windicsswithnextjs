a50ec71a1b572cccc3ab1be1b18ea201f1be1d44
officialrajdeepsingh <radewala.2@gmail.com>
2022-11-18 15:58:03 +0530

    add default config come with nextjs

--- next.config.js
+++ next.config.js
@@ -4,9 +4,11 @@ const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
 
 module.exports = {
-
+  reactStrictMode: true,
+  swcMinify: true,
   webpack(config) {
     config.plugins.push(new WindiCSSWebpackPlugin())
     return config
   }
+
 }
